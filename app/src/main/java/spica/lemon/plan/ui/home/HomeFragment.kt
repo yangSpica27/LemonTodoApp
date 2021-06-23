187fb612515ed84a72b56657eefdd29caee1f518
Spica27 <spica27@qq.com>
2021-06-23 22:20:17 +0800

    # 登录页面优化 # 增加待办集合

--- app/src/main/java/spica/lemon/plan/ui/home/HomeFragment.kt
+++ app/src/main/java/spica/lemon/plan/ui/home/HomeFragment.kt
@@ -2,13 +2,18 @@ package spica.lemon.plan.ui.home
 
 import android.view.*
 import androidx.fragment.app.viewModels
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.bottomsheets.BottomSheet
+import com.afollestad.materialdialogs.bottomsheets.setPeekHeight
+import com.afollestad.materialdialogs.customview.customView
+import com.afollestad.materialdialogs.lifecycle.lifecycleOwner
 import com.drakeet.multitype.MultiTypeAdapter
 import com.fondesa.recyclerviewdivider.dividerBuilder
 import spica.lemon.plan.R
 import spica.lemon.plan.base.BaseFragment
 import spica.lemon.plan.databinding.FragmentHomeBinding
+import spica.lemon.plan.databinding.LayoutFolderBinding
 import spica.lemon.plan.model.Schedule
-import spica.lemon.plan.view.FolderDialog
 
 /**
  * 主页
@@ -21,30 +26,62 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
   private val homeViewModel: HomeViewModel by viewModels()
 
+  private lateinit var dialogBinding: LayoutFolderBinding
 
-  private lateinit var folderDialog: FolderDialog
+
+  private lateinit var folderDialog: MaterialDialog
 
   override fun setupViewBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentHomeBinding =
     FragmentHomeBinding.inflate(inflater, container, false)
 
 
-
   override fun init() {
-    context
-    folderDialog = FolderDialog()
-    adapter = MultiTypeAdapter(items)
-    adapter.register(HomeListDelegate(requireActivity()))
-    adapter.setHasStableIds(true)
+    initDialog()
+    initToolbar()
+    initRecyclerView()
+  }
 
+
+  /**
+   * 初始化顶栏
+   */
+  private fun initToolbar() {
     viewBinding.toolbar.setOnMenuItemClickListener {
       when (it.itemId) {
         R.id.action_folder -> {
+          folderDialog.show()
           return@setOnMenuItemClickListener true
         }
       }
       return@setOnMenuItemClickListener false
     }
+  }
 
+  /**
+   * 初始化底部弹窗
+   */
+  private fun initDialog() {
+    dialogBinding = LayoutFolderBinding.inflate(LayoutInflater.from(requireContext()))
+    folderDialog = MaterialDialog(requireContext(), BottomSheet())
+    folderDialog.customView(
+      view = dialogBinding.root, noVerticalPadding = true,
+      dialogWrapContent = true, scrollable = false
+    )
+    folderDialog.cornerRadius(4F)
+    folderDialog.setPeekHeight(500.dp)
+    folderDialog.lifecycleOwner(viewLifecycleOwner)
+    dialogBinding.btnCancel.setOnClickListener {
+      folderDialog.dismiss()
+    }
+  }
+
+  /**
+   * 初始化主页列表
+   */
+  private fun initRecyclerView() {
+    adapter = MultiTypeAdapter(items)
+    adapter.register(HomeListDelegate(requireActivity()))
+    adapter.setHasStableIds(true)
     requireContext()
       .dividerBuilder()
       .showLastDivider()
--- app/src/main/java/spica/lemon/plan/ui/profile/ProfileFragment.kt
+++ app/src/main/java/spica/lemon/plan/ui/profile/ProfileFragment.kt
@@ -1,23 +1,49 @@
 package spica.lemon.plan.ui.profile
 
-import android.content.Intent
+
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.bottomsheets.BottomSheet
+import com.afollestad.materialdialogs.bottomsheets.setPeekHeight
+import com.afollestad.materialdialogs.customview.customView
+import com.afollestad.materialdialogs.lifecycle.lifecycleOwner
 import spica.lemon.plan.base.BaseFragment
+import spica.lemon.plan.databinding.DialogLoginBinding
 import spica.lemon.plan.databinding.FragmentProfileBinding
-import spica.lemon.plan.ui.login.LoginActivity
+
 
 /**
  * 个人配置页
  */
 class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
+
+  private lateinit var loginDialog: MaterialDialog
+
+  private lateinit var loginBinding: DialogLoginBinding
+
+
   override fun setupViewBinding(inflater: LayoutInflater, container: ViewGroup?): FragmentProfileBinding =
     FragmentProfileBinding.inflate(inflater, container, false)
 
   override fun init() {
+    initLoginDialog()
     viewBinding.ivAvatar.setOnClickListener {
-      val intent = Intent(requireContext(), LoginActivity::class.java)
-      requireActivity().startActivity(intent)
+      loginDialog.show()
+    }
+  }
+
+
+  /**
+   * 初始化登录dialog
+   */
+  private fun initLoginDialog() {
+    loginBinding = DialogLoginBinding.inflate(LayoutInflater.from(requireContext()))
+    loginDialog = MaterialDialog(requireContext(), BottomSheet())
+    with(loginDialog) {
+      customView(view = loginBinding.root, scrollable = true, noVerticalPadding = true)
+      setPeekHeight(500.dp)
+      lifecycleOwner(viewLifecycleOwner)
     }
   }
 
